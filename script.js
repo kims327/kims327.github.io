45a2a34a620951dff26fe7d4656f9352f81fa549
alicevahn <kims327@newschool.edu>
2021-03-25 22:02:36 +0900

    index update

---
+++ script.js
@@ -0,0 +1,26 @@
+// document.getElementById('background').onclick = changeColor;
+//
+//  function changeColor() {
+//      document.body.style.color = "purple";
+//      return false;
+//  }
+//
+
+//  function ClickMe() {
+// document.querySelector('body').style.backgroundColor="yellow";
+// return false;
+// }
+
+document.querySelector('#button').onclick = function(){
+document.querySelector('body').style.backgroundColor="yellow";
+document.querySelector('h1').style.color="blue";
+document.querySelector('#button').style.color="blue";
+}
+
+
+// document.querySelector('#button2').onclick = function(){
+// document.querySelector('body').style.backgroundColor="yellow";
+// document.querySelector('h1').style.color="blue";
+// document.querySelector('#button2').style.color="blue";
+// document.querySelector('#button2').style.display="none";
+// }
